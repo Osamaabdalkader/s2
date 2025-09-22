a3456428148cd24a73de6969383eb021ef880629
Osamaabdalkader <osamaabdalkader100@gmail.com>
2025-09-22 03:17:02 +0300

    Update supbase.js

--- js/supbase.js
+++ js/supbase.js
@@ -1,6 +1,6 @@
 // تهيئة Supabase
 const SUPABASE_URL = 'https://rrjocpzsyxefcsztazkd.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyam9jcHpzeXhlZmNzenRhemtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyOTEzMTgsImV4cCI6MjA3Mzg2NzMxOH0.TvUthkBc_lnDdGlHJdEFUPo4Dl2n2oHyokXZE8_wodw';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhbmFzZSIsInJlZiI6InJyam9jcHpzeXhlZmNzenRhemtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyOTEzMTgsImV4cCI6MjA3Mzg2NzMxOH0.TvUthkBc_lnDdGlHJdEFUPo4Dl2n2oHyokXZE8_wodw';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 let debugMode = false;
@@ -31,4 +31,4 @@ async function testConnection() {
         connectionStatus.textContent = `خطأ في الاتصال: ${error.message}`;
         connectionStatus.className = 'connection-status connection-error';
     }
-                    }
+            }
